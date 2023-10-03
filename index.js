7dc854e06cc5b3e43d991836b572e8353dc36b49
Sarbagya Dhaubanjar <sarbagyastha@gmail.com>
2023-10-03 15:53:27 +0545

    fixed timezone

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import { getInput, setOutput, setFailed } from "@actions/core";
 import { context } from "@actions/github";
 import { format } from "date-fns";
+import { utcToZonedTime } from "date-fns-tz";
 
 function currentTimestamp(timeZone) {
-  const now = new Date();
+  const now = utcToZonedTime(new Date(), timeZone);
 
   return format(now, "yyMMddHHm", {
     timeZone: timeZone,
@@ -13,12 +14,8 @@ function currentTimestamp(timeZone) {
 try {
   const timezone = getInput("timezone");
   console.log(`Timezone: ${timezone}`);
-  const time = new Date().toTimeString();
-  setOutput("build-number", currentTimestamp(timezone));
 
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(context.payload, undefined, 2);
-  console.log(`The event payload: ${payload}`);
+  setOutput("build-number", currentTimestamp(timezone));
 } catch (error) {
   setFailed(error.message);
 }
